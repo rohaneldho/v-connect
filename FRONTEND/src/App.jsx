93f8eda9ca478ce7fc7639b64ba9a532e7fb1f55
Rohan Eldho <138814144+rohaneldho@users.noreply.github.com>
2024-04-09 00:59:32 +0530

    Trials

--- FRONTEND/src/App.jsx
+++ FRONTEND/src/App.jsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState , useEffect } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+const App = () => {
+  const [token, setToken] = useState(false)
+
+  useEffect(() => {
+    if (token) {
+      const expiration = new Date()
+      expiration.setTime(expiration.getTime() + 1 * 60 * 60 * 1000)
+      const expires = 'expires=' + expiration.toUTCString()
+      document.cookie = `token=${token}; ${expires}; path=/`
+    }
+  }, [token])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+    <div>
+      <p>
+        hello
       </p>
-    </>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/login" element={<Login setToken={setToken} />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/createSchedule" element={<Createschedule />} />
+        <Route path="/history" element={<Task />} />
+        <Route path="/delete" element={<Delete />} />
+        <Route path="/setAdmin" element={<SetAdmin />} />
+        <Route path="/remove" element={<Remove />} />
+        <Route path="/uploadFile" element={<Upload />} />
+      </Routes>
+    </div>
   )
 }
 
